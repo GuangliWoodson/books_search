4de30aeaa08c1344dd030dcca87b1c0e34e776be
GuangliWoodson <guangliwoodson@outlook.com>
2022-07-21 11:54:29 +0800

    test_01

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import searchInput from './components/search_input'
+import booksList from './components/books_list'
+import boosAdd from './components/books_add'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    searchInput,
+    booksList,
+    booksAdd
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
